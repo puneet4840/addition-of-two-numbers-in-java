ec3777d2f04b6e09f191e791c2b56073853e24cf
Puneet Verma <puneet.verma02@nagarro.com>
2023-10-17 22:45:02 +0530

    Sum function

---
+++ main.java
@@ -0,0 +1,9 @@
+public class SumOfNumbers1  
+{  
+    public static void main(String args[])   
+    {  
+        int n1 = 10, n2 = 20, sum;  
+        sum = n1 + n2;  
+        System.out.println("The sum of numbers is: "+sum);  
+    }  
+}  
